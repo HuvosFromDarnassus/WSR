39fc8a3694063c01fbd28299fd48942d8c9092df
Daniel Tvorun <danya.tvorun.88@mail.ru>
2020-11-10 22:02:57 +0300

    Исправление размеров изображений товаров

---
+++ js/news-scroll.js
@@ -0,0 +1,32 @@
+function vscroll_data0() {
+  this.enable_visual_design_mode = true;
+
+  this.container_width = 160;
+  this.container_height = 190;
+  this.top_pause_offset = 10;
+
+  /*---------------------------------------------
+   Timing and Animation Speed
+   ---------------------------------------------*/
+
+  this.initial_scroll_delay = 0;
+
+  this.animation_delay = 20;
+  this.animation_jump = 2;
+
+  this.animation_delay_mac = 100;
+  this.animation_jump_mac = 10;
+
+  /*---------------------------------------------
+   Scroll Styles
+   ---------------------------------------------*/
+
+  this.container_styles =
+    "background-color:#f5f5f5; background-image:; border-style:dashed; border-color:#000000; border-width:1px; padding-right:10px; padding-left:10px; ";
+  this.item_link_styles =
+    "margin-bottom:0px; background-color:; background-image:; color:#1e51c8; text-align:; font-family:Arial; font-size:13px; font-weight:; text-decoration:none; border-style:none; border-color:; border-width:0px; padding-top:; padding-right:; padding-bottom:; padding-left:; ";
+  this.item_link_hover_styles =
+    "background-color:; background-image:; color:; text-align:; font-family:Arial; font-size:; font-weight:; text-decoration:underline; ";
+  this.item_styles =
+    "margin-bottom:15px; background-color:; background-image:; color:#212e43; text-align:; font-family:Arial; font-size:13px; font-weight:; text-decoration:none; ";
+}
